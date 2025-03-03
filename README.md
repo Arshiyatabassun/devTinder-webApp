df68185af3405211b50ffa65b6b96d4dff652a98
Arshiya Tabassum <96403942+Arshiyatabassun@users.noreply.github.com>
2025-03-03 23:27:51 +0530

    axios added

--- README.md
+++ README.md
@@ -7,4 +7,9 @@
 - create a separate NavBar.jsx component file
 - Install react-router-dom
 - create BrowserRouter,Routes,Route=>/Body=>ChildrenRoute
-- Create an Outlet inn your Body Component
+- Create an Outlet inn your Body Component.
+- create a footer component
+ ## Ep-16
+ - Install axios
+ -CORS-install cors in backend =>add middlewear to with configuration origin,credentials:true
+ - whenever your making an API calls you pass axios =>{withCredentials:true}
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,49 @@
+import { useState } from "react";
+import axios from 'axios';
+
 const Login =()=>{
+    const [emailId ,setEmailId]=useState("simran@gmail.com");
+    const [password,setPassword]=useState("Simran@123");
+
+   const handleClick= async()=>{
+    try{
+        const res = await axios.post("http://localhost:3000/login",{
+            emailId,
+            password,
+        },{withCredentials:true});
+    }catch(err){
+        console.log(err)
+    }
+
+    };
     return (
-        <>
-       <h1>Login</h1> 
-        </>
+        <div className="my-10 flex justify-center">
+    <div className="card bg-base-300 w-96 shadow-sm">
+  <div className="card-body">
+    <h2 className="card-title  justify-center">Login</h2>
+    <div>
+    <fieldset className="fieldset">
+  <legend className="fieldset-legend ">Email ID:</legend>
+  <input type="text" value={emailId} className="input" onChange={(e)=> setEmailId(e.target.value)} />
+ 
+</fieldset>
+
+    </div>
+    <div>
+    <fieldset className="fieldset">
+  <legend className="fieldset-legend">Password:</legend>
+  <input type="text"  value ={password} onChange ={(e)=>setPassword(e.target.value)} className="input" />
+ 
+</fieldset>
+
+    </div>
+
+    <div className="card-actions justify-center">
+      <button className="btn btn-primary my-2" onClick={handleClick}>Login</button>
+    </div>
+  </div>
+</div>
+        </div>
     )
 }
 export default Login;
